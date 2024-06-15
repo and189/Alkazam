635106e035807d8dfd3403204a8fe58c05047b03
and189 <and1_89@live.de>
2024-06-15 17:31:35 +0100

    Update alkazam.sh

--- alkazam.sh
+++ alkazam.sh
@@ -9,6 +9,7 @@ proxy_port="80"
 proxy_username="ProxyUSER"
 proxy_password="ProxyPW"
 
+
 counter_file="counter.txt"
 accounts_file="accounts.txt"
 username_counter=3
@@ -24,47 +25,40 @@ if [ -f "$counter_file" ]; then
 fi
 
 open_browser_with_proxy_auth() {
-    # Öffne den Brave-Browser im privaten Modus mit Proxy-Einstellungen und lade eine einfache Seite
     /snap/brave/416/opt/brave.com/brave/brave-browser --incognito --proxy-server="http://$proxy_host:$proxy_port" "http://www.google.com" --enable-logging --v=1 &
     sleep 5
-
-    # Warte auf das Proxy-Authentifizierungsfenster und fokussiere es
     xdotool search --sync --onlyvisible --class "Brave-browser" windowactivate
-
-    # Gebe Proxy-Authentifizierungsdaten ein
-    sleep 1  # kurze Pause, um sicherzustellen, dass das Fenster bereit ist
+    sleep 2
     xdotool type "$proxy_username"
     xdotool key Tab
     xdotool type "$proxy_password"
     xdotool key Return
-    sleep 3
+    sleep 1
 }
 
 open_new_tab_with_url() {
     url=$1
-    # Öffne eine neue Registerkarte im bestehenden Brave-Browser
     xdotool search --onlyvisible --class brave windowactivate --sync key --clearmodifiers ctrl+t
-    sleep 1
+    sleep 2
     echo -n "$url" | xclip -selection clipboard
     xdotool key ctrl+l
     xdotool key ctrl+v
     xdotool key Return
-    sleep 5
+    sleep 4
 }
 
 reload_tab() {
-    # Lade die aktuelle Registerkarte neu
     xdotool key ctrl+r
-    sleep 3
+    sleep 5
 }
 
 check_page_loaded() {
-    # Überprüfe, ob die Seite geladen wurde, indem nach einem bestimmten Text gesucht wird
     xdotool key ctrl+a
+    sleep 1
     xdotool key ctrl+c
     sleep 1
     text=$(xclip -o)
-    echo "Loaded text: $text"  # Debugging-Information
+    echo "Loaded text: $text"
     if [[ "$text" == *"Your username will only be used to log in."* ]]; then
         return 0
     elif [[ "$text" == *"Oops! This username is not available"* ]]; then
@@ -77,12 +71,12 @@ check_page_loaded() {
 }
 
 check_username_availability() {
-    # Überprüfe, ob der Benutzername verfügbar ist
     xdotool key ctrl+a
+    sleep 1
     xdotool key ctrl+c
     sleep 1
     text=$(xclip -o)
-    echo "Username availability text: $text"  # Debugging-Information
+    echo "Username availability text: $text"
     if [[ "$text" == *"Oops! This username is not available"* ]]; then
         return 1
     else
@@ -90,14 +84,18 @@ check_username_availability() {
     fi
 }
 
-close_tab() {
-    # Schließe die aktuelle Registerkarte
-    xdotool key ctrl+w
+focus_back_to_username_field() {
+    # Beispielkoordinaten, die auf das Benutzernamenfeld zeigen - diese müssen möglicherweise angepasst werden
+    xdotool mousemove 300 300 click 1
     sleep 1
 }
 
+close_tab() {
+    xdotool key ctrl+w
+    sleep 2
+}
+
 close_browser() {
-    # Schließt den Browser
     pkill brave
     sleep 5
 }
@@ -111,96 +109,88 @@ open_browser_with_proxy_auth
 
 for (( i=0; i<1000; i++ )); do
     while true; do
-        # Berechne den aktuellen Benutzernamen
         username="${base_username}${username_counter}"
         password="${base_password}"
-
-        # Berechne die aktuelle E-Mail-Adresse
         email_parts=(${base_email//@/ })
         local_part=${email_parts[0]}
         domain=${email_parts[1]}
-
         hyphen_index=$(expr index "$local_part" "-")
         base_local=${local_part:0:$hyphen_index}
         current_number=$email_counter
         email="${base_local}${current_number}@${domain}"
-
-        # Öffne einen neuen Tab und lade die Seite
         url="https://sso.niantic.kidswebservices.com/en/register?clientId=pokemon-go&automaticActivation=true&skipRedirect=true&hideSignIn=true&permissionsToRequest=location&dob=1452343200000"
         open_new_tab_with_url "$url"
-
-        # Überprüfe, ob die Seite geladen wurde
         check_page_loaded
         page_status=$?
         if [ $page_status -eq 0 ]; then
             break
         elif [ $page_status -eq 2 ]; then
-            # Wenn "Oops!" beim Laden der Seite gefunden wird, Browser neu starten
             restart_browser
         else
             restart_browser
         fi
     done
 
-    while true; do
-        # Fülle das Formular aus
-        xdotool key Tab
-        xdotool type "$username"
-        xdotool key Tab
-        xdotool key Tab
-        xdotool type "$password"
-        xdotool key Tab
-        sleep 1
-        xdotool key Tab
-        xdotool key Tab
-        xdotool key Tab
-
-        # Überprüfe, ob der Benutzername verfügbar ist
-        check_username_availability
-        if [ $? -eq 0 ]; then
-            break
-        else
-            # Benutzername nicht verfügbar, inkrementiere den Zähler und lade den Tab neu
-            username_counter=$((username_counter + 1))
-            reload_tab
-        fi
-    done
-
-    xdotool key Return
+    # Fülle das Formular aus und überprüfe die Verfügbarkeit des Benutzernamens
+    xdotool key Tab
+    xdotool type "$username"
+    sleep 1
+    xdotool key Tab
+    xdotool key Tab
+    xdotool type "$password"
+    sleep 1
+    xdotool key Tab
+    xdotool key Tab
     xdotool key Tab
     sleep 1
+    # Text markieren und auswerten
+    xdotool key ctrl+a
+    sleep 2
+    xdotool key ctrl+c
+    sleep 2
+    text=$(xclip -o)
+    echo "Username availability text: $text"
+    if [[ "$text" == *"Oops! This username is not available"* ]]; then
+        username_counter=$((username_counter + 1))
+        echo -e "$username_counter\n$email_counter\n$created_accounts" > "$counter_file"
+        continue  # Zum nächsten Benutzernamen wechseln
+    fi
+    xdotool key Tab
+    xdotool key Tab
+    xdotool key Tab
+    xdotool key Tab
+    xdotool key Tab
+    xdotool key Tab
+    xdotool key Tab
+    sleep 2
+    xdotool key Return
+    sleep 5
+    xdotool key Tab
     xdotool type "$email"
     xdotool key Return
     sleep 5
-
-    # Überprüfe, ob die "WOO!"-Seite geladen wurde
+    xdotool key Tab
+    xdotool type "$email"
+    xdotool key Return
     check_page_loaded
     page_status=$?
     if [ $page_status -eq 3 ]; then
-        # Füge den erstellten Account zur Datei hinzu
-        echo "${username};${password}" >> "$accounts_file"
-
-        # Inkrementiere die Zähler
-        username_counter=$((username_counter + 1))
+        echo "${username};${password};${email}" >> "$accounts_file"
         created_accounts=$((created_accounts + 1))
-
-        # Erhöhe den email_counter nur alle 50 erstellten Konten
         if [ "$created_accounts" -ge 50 ]; then
             email_counter=$((email_counter + 1))
             created_accounts=0
         fi
-
-        # Schließe den aktuellen Tab
+        echo -e "$username_counter\n$email_counter\n$created_accounts" > "$counter_file"
+        username_counter=$((username_counter + 1))
         close_tab
     else
+        username_counter=$((username_counter + 1))
         restart_browser
     fi
 
-    # Schließe den Browser und starte ihn alle 49 Iterationen neu
-    if [ $((i % 49)) -eq 0 ]; then
+    if [ $((i % 50)) -eq 0 ]; then
         restart_browser
     fi
-
-    # Speichere die aktuellen Zählerstände in die Datei
-    echo -e "$username_counter\n$email_counter\n$created_accounts" > "$counter_file"
 done
+
